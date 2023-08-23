6b4ff3832c2cda6447851effd4cca409a895de21
taisei <taiseisunaga@gmail.com>
2023-08-24 02:29:43 +0900

    update

--- js/function.js
+++ js/function.js
@@ -1,83 +1,104 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const cells = document.querySelectorAll(".js-cell");
-    const stateMessage = document.querySelector(".js-state-message");
-    const restartButton = document.querySelector(".js-restart");
-    const turnItems = document.querySelectorAll(".turn-item");
-  
-    let currentPlayer = "circle"; // "circle" or "cross"
-    let gameBoard = ["", "", "", "", "", "", "", "", ""]; // Represents the game board
-  
-    // Function to handle cell clicks
-    function handleCellClick(index) {
-        if (gameBoard[index] === "" && !stateMessage.textContent) {
-          gameBoard[index] = currentPlayer;
-          cells[index].classList.add(currentPlayer);
-          cells[index].textContent = currentPlayer === "circle" ? "○" : "×"; // Display symbol
-          checkWin();
-          togglePlayer();
-        }
-      }
-  
-    // Function to toggle current player
-    function togglePlayer() {
-      currentPlayer = currentPlayer === "circle" ? "cross" : "circle";
-      turnItems.forEach(item => item.classList.toggle("active"));
-    }
-  
-    // Function to check if someone wins
-    function checkWin() {
-      const winCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6] // Diagonals
-      ];
-  
-      for (const combo of winCombos) {
-        const [a, b, c] = combo;
-        if (gameBoard[a] && gameBoard[a] === gameBoard[b] && gameBoard[a] === gameBoard[c]) {
-          stateMessage.textContent = `${gameBoard[a].toUpperCase()} wins!`;
-        }
-      }
-  
-      if (!gameBoard.includes("") && !stateMessage.textContent) {
-        stateMessage.textContent = "It's a draw!";
-      }
-    }
-  
-    // Function to restart the game 
-    function restartGame() {
-      gameBoard = ["", "", "", "", "", "", "", "", ""];
-      cells.forEach(cell => {
-        cell.classList.remove("circle", "cross");
-      });
-      stateMessage.textContent = "";
-      currentPlayer = "circle";
-      turnItems[0].classList.add("active");
-    }
-  
-    // Event listeners
-    cells.forEach((cell, index) => {
-      cell.addEventListener("click", () => handleCellClick(index));
-    });
-  
-    restartButton.addEventListener("click", restartGame);
+const context = {
+  count: 0,
+  isCircleTurn: true,
+  progress: true,
+  cells: new Array(9),
+  cellElements: document.querySelectorAll(".js-cell"),
+  circleElement: document.querySelector(".turn-item.circle"),
+  crossElement: document.querySelector(".turn-item.cross"),
+  stateMessageElement: document.querySelector(".js-state-message"),
+  restartButtonElement: document.querySelector(".js-restart")
+}
 
-      // Function to restart the game
-  function restartGame() {
-    gameBoard = ["", "", "", "", "", "", "", "", ""];
-    cells.forEach(cell => {
-      cell.classList.remove("circle", "cross");
-      cell.textContent = ""; // Clear the cell content
-    });
-    stateMessage.textContent = "";
-    currentPlayer = "circle";
-    turnItems[0].classList.add("active");
+//勝ちパターンの組み合わせ
+const winningCombinations = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 3, 6],
+[1, 4, 7],
+[2, 5, 8],
+[0, 4, 8],
+[2, 4, 6]
+] 
+
+
+const ACTIVE_CLASSNAME = "active";
+
+// メッセージ
+const STATUS = {
+  start: "starting...",
+  win: "%name% win!!",
+  draw: "draw" 
+}
+const CHARACTERS = {
+  circle: "○",
+  cross: "×"
+}
+
+function toggleTurn(circleElement, crossElement) {
+  circleElement.classList.toggle(ACTIVE_CLASSNAME);
+  crossElement.classList.toggle(ACTIVE_CLASSNAME);
+}
+
+function checkWinner(context) {
+// 1つでもwinningCombinations内の配列と一致すればtrueを返す
+return winningCombinations.some(pattern => {
+  const { cells } = context;
+  // 1つ目の記号 ○ or ×
+  const first = cells[pattern[0]];
+  // 2つ目の記号 ○ or ×
+  const second = cells[pattern[1]];
+  // 3つ目の記号 ○ or ×
+  const third = cells[pattern[2]];
+          
+  // WINNING_PATTERNのcellの1つ目=2つ目、1つ目=3つ目が全て ○ or × で一致した場合にtrueを返す
+  return first && first === second && first === third;
+})
+}
+
+// 表内のクリック時
+function onClickCell(e) {
+  const { cells, progress, isCircleTurn, stateMessageElement, circleElement, crossElement } = context;
+  const index = e.target.getAttribute("data-key") - 1;
+
+  // cellに値が入っている、またはゲーム中の場合
+  if(cells[index] || !progress) {
+      return;
   }
 
-  // Event listeners
-  cells.forEach((cell, index) => {
-    cell.addEventListener("click", () => handleCellClick(index));
-  });
+  // ○×書き込み
+  const value = isCircleTurn ? CHARACTERS.circle : CHARACTERS.cross;
+  e.target.innerHTML = value;
+  cells[index] = value;
 
-  restartButton.addEventListener("click", restartGame);
-});
+  // どちらかが勝った場合
+  if(checkWinner(context)) {
+      context.progress = false;
+      const message = isCircleTurn ? STATUS.win.replace("%name%", CHARACTERS.circle) : STATUS.win.replace("%name%", CHARACTERS.cross);
+      stateMessageElement.innerHTML = message;
+      return;
+  }
+
+  // 引き分けの場合
+  context.count++;
+  if(context.count === 9) {
+      context.progress = false;
+      stateMessageElement.innerHTML = STATUS.draw;
+      return;
+  }
+
+  // ターン変更
+  toggleTurn(circleElement, crossElement);
+  context.isCircleTurn = !isCircleTurn;
+}
+
+function subscribe() {
+  context.stateMessageElement.innerHTML = STATUS.start;
+  context.cellElements.forEach(item => {
+      item.addEventListener("click", onClickCell)
+  })
+  context.restartButtonElement.addEventListener("click", () => location.reload());
+}
+
+subscribe();
